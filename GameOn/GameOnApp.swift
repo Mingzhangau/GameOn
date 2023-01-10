cf84de1b72bb881f6438a4fad2b77562daa6e84a
Ming Zhang <ming.zhang@thelotterycorporation.com>
2023-01-10 15:31:05 +1000

    Initial Commit

---
+++ GameOn/GameOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOnApp.swift
+//  GameOn
+//
+//  Created by Ming Zhang on 10/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
