d33ebd5a6f3b6e0c781c8bd5a9d31ba16df6a871
Ming Zhang <ming.zhang@thelotterycorporation.com>
2023-10-20 10:59:57 +1000

    change team

--- GameOn/Models/GameViewModel.swift
+++ GameOn/Models/GameViewModel.swift
@@ -15,27 +15,33 @@ extension SettingsView {
         @Published
         var teams: [Team] = [
             Team(id: "Manchester Nunited", isInOptions: true),
-            Team(id: "Manchester City", isInOptions: true),
-            Team(id: "Liverpool", isInOptions: true),
+
+
             Team(id: "Arsenal", isInOptions: true),
-            Team(id: "HotSpur", isInOptions: true),
-            Team(id: "Chelsea", isInOptions: true),
+
             Team(id: "Paris Saint-Germain", isInOptions: true),
-            Team(id: "AS Monaco", isInOptions: true),
+
             Team(id: "Inter Milano", isInOptions: true),
             Team(id: "AC Milan", isInOptions: true),
-            Team(id: "Juventus (Piemonte BN)", isInOptions: true),
             Team(id: "Lazio", isInOptions: true),
             Team(id: "Roma", isInOptions: true),
             Team(id: "Napoli", isInOptions: true),
-            Team(id: "Real Madrid", isInOptions: true),
+
             Team(id: "Barcelona", isInOptions: true),
-            Team(id: "Madrid Rosas", isInOptions: true),
+
             Team(id: "Bayern Munich", isInOptions: true),
 
-            Team(id: "Atletico Mineiro (Brasil)", isInOptions: true),
-            Team(id: "Flamengo", isInOptions: true),
-            Team(id: "River Plate", isInOptions: true)
+            Team(id: "Germany", isInOptions: true),
+            Team(id: "England", isInOptions: true),
+            Team(id: "Holland", isInOptions: true),
+            
+            Team(id: "France", isInOptions: true),
+            Team(id: "Italian", isInOptions: true),
+            Team(id: "Croatia", isInOptions: true),
+            Team(id: "Spanish", isInOptions: true),
+            Team(id: "Portugal", isInOptions: true),
+            Team(id: "Argentina", isInOptions: true)
+
         ]
 
         @Published
